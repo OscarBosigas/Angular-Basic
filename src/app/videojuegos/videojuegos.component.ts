fadad3c528c93373c413d4c0091ae75d88af708a
Oscar Bosigas <oscar.bosigas@uptc.edu.co>
2019-03-28 16:00:32 -0500

    ngFor, vistas individuales

--- src/app/videojuegos/videojuegos.component.ts
+++ src/app/videojuegos/videojuegos.component.ts
@@ -2,21 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'videojuegos',
-  template: `
-    <h2>Componente de: {{nombre}}</h2>
-
-    <h3 [style.background]="'red'"
-     *ngIf= "mostrar_juego_retro != true" >El mejor juego es: {{juego}}</h3>
-
-    <h3 [style.background]="color2"
-     *ngIf= "mostrar_juego_retro" >El mejor juego es: {{juego_retro}}</h3>
-  `
+  templateUrl: './videojuegos.component.html'
 })
 
 export class VideojuegosComponent{
-  public nombre = 'Videojuegos 2018';
-  public juego = 'Halo2';
-  public juego_retro = "Mario 64";
-  public mostrar_juego_retro = false;
-  public color2 = "blue";
+  public nombre:string;
+  public juego:string;
+  public juego_retro:string;
+  public mostrar_juego_retro:boolean;
+  public color2:string;
+  public anio:number;
+
+  public juegos:Array<string>;
+  constructor(){
+    this.nombre = 'Videojuegos 2018';
+    this.juego = 'Halo2';
+    this.juego_retro = "Mario 64";
+    this.mostrar_juego_retro = false;
+    this.color2 = "blue";
+    this.anio = 2018;
+    this.juegos = ['San Andreas', 'NFSMW', 'Call of duty', 'Fornite'];
+
+  }
 }
