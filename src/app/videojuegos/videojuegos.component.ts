3f51592b102b8ad984115b294e16c9ac26e2715d
Oscar Bosigas <oscar.bosigas@uptc.edu.co>
2019-03-28 15:16:32 -0500

    ngIf y estilos en linea

--- src/app/videojuegos/videojuegos.component.ts
+++ src/app/videojuegos/videojuegos.component.ts
@@ -4,11 +4,19 @@ import { Component } from '@angular/core';
   selector: 'videojuegos',
   template: `
     <h2>Componente de: {{nombre}}</h2>
-    <h3>El mejor juego es: {{juego}}</h3>
+
+    <h3 [style.background]="'red'"
+     *ngIf= "mostrar_juego_retro != true" >El mejor juego es: {{juego}}</h3>
+
+    <h3 [style.background]="color2"
+     *ngIf= "mostrar_juego_retro" >El mejor juego es: {{juego_retro}}</h3>
   `
 })
 
 export class VideojuegosComponent{
   public nombre = 'Videojuegos 2018';
   public juego = 'Halo2';
+  public juego_retro = "Mario 64";
+  public mostrar_juego_retro = false;
+  public color2 = "blue";
 }
